de9de8cfd728147282c8cc3d9ad1e8ae839d69d5
developing-josh-13 <joshmeikle86@gmail.com>
2026-07-05 21:04:06 +0100

    First commit of my files as I have them so far

---
+++ quizzes/foodAndDrink/foodAndDrinkQuiz.js
@@ -0,0 +1,76 @@
+
+
+const questions = [
+	{ // Q1
+		question: 'What is the official corporate logo for Guinness?',
+		answers: [
+			{answer: 'Banjo', correct: 'no'},
+			{answer: 'Triangle', correct: 'no'},
+			{answer: 'Harp', correct: 'yes'},
+			{answer: 'Cello', correct: 'no'}
+		]
+	},
+	{ // Q2
+		question: 'In which country are the self-proclaimed \'master chocolatiers\' Lindt based?',
+		answers: [
+			{answer: 'Switzerland', correct: 'yes'},
+			{answer: 'Belgium', correct: 'no'},
+			{answer: 'France', correct: 'no'},
+			{answer: 'Sweden', correct: 'no'}
+		]
+	},
+	{ // Q3
+		question: 'Which fast-food establishment\'s long-held slogan is "Eat Fresh"?',
+		answers: [
+			{answer: 'Popeye\'s', correct: 'no'},
+			{answer: 'KFC', correct: 'no'},
+			{answer: 'Taco Bell', correct: 'no'},
+			{answer: 'Subway', correct: 'yes'}
+		]
+	},
+	{ // Q4
+		question: 'Which country produces the most coffee in the world?',
+		answers: [
+			{answer: 'Mexico', correct: 'no'},
+			{answer: 'Brazil', correct: 'yes'},
+			{answer: 'Argentina', correct: 'no'},
+			{answer: 'Colombia', correct: 'no'}
+		]
+	},
+	{ // Q5
+		question: 'Which is the oldest soft drink to be served in America?',
+		answers: [
+			{answer: 'Coca Cola', correct: 'no'},
+			{answer: 'Fanta', correct: 'no'},
+			{answer: 'Dr Pepper', correct: 'yes'},
+			{answer: 'Sprite', correct: 'no'}
+		]
+	}
+]
+
+// const randomNum = Math.floor((Math.random() * 6));
+// console.log(randomNum);
+
+// if (randomNum) {
+// 	document.getElementById("q1").innerHTML = questions[randomNum].question;
+// }
+// const testElement = document.getElementById("q1");
+// console.log(testElement);
+
+// const testText = questions[randomNum].question;
+// console.log(testText);
+
+
+const correctAnswers = [];
+
+questions.forEach(questionObj => {
+	const correctAnswer = questionObj.answers.find(answer => answer.correct == "yes");
+	correctAnswers.push(correctAnswer);
+});
+
+// console.log(correctAnswers);
+
+// const ans1 = document.getElementById("ans1");
+// const ans2 = document.getElementById("ans2");
+// const ans3 = document.getElementById("ans3");
+// const ans4 = document.getElementById("ans4");
---
+++ quizzes/maths/mathsQuiz.js
@@ -0,0 +1,47 @@
+const questions = [
+	{ // Q1
+		question: 'What is the name of an angle between 180 and 360 degrees?',
+		answers: [
+			{answer: 'Tangent', correct: 'no'},
+			{answer: 'Acute', correct: 'no'},
+			{answer: 'Obtuse', correct: 'no'},
+			{answer: 'Reflex', correct: 'yes'}
+		]
+	},
+	{ // Q2
+		question: 'How many lines are needed to sketch a cube?',
+		answers: [
+			{answer: '7', correct: 'no'},
+			{answer: '8', correct: 'no'},
+			{answer: '9', correct: 'yes'},
+			{answer: '10', correct: 'no'}
+		]
+	},
+	{ // Q3
+		question: 'What is the formula for Pythagoras\'s theorem to calculate the longest side of a right-angled triangle?',
+		answers: [
+			{answer: 'a + b = c^2', correct: 'no'},
+			{answer: 'a^2 + b^2 = c^2', correct: 'yes'},
+			{answer: '(a+b)^2 = c^2', correct: 'no'},
+			{answer: 'a^3 + b^3 = c^3', correct: 'no'}
+		]
+	},
+	{ // Q4
+		question: 'How many millimetres are in a kilometre?',
+		answers: [
+			{answer: '10 thousand', correct: 'no'},
+			{answer: '100 thousand', correct: 'no'},
+			{answer: '1 million', correct: 'yes'},
+			{answer: '10 million', correct: 'no'}
+		]
+	},
+	{ // Q5
+		question: 'Which of the following is a prime number?',
+		answers: [
+			{answer: '57', correct: 'no'},
+			{answer: '67', correct: 'yes'},
+			{answer: '77', correct: 'no'},
+			{answer: '87', correct: 'no'}
+		]
+	}
+]
